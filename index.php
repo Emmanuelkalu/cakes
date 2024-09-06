9ccc56e8c2ea610e7b72c2aee222154261f49e37
Henschel <henschly@gmail.com>
2024-09-06 07:57:41 +0100

    inityialcommit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$requestUri = $_SERVER['REQUEST_URI'];
+$filePath = ltrim($requestUri, '/kek');
+
+if (empty($filePath)) {
+    include 'home.php';
+} else {
+
+
+    if (file_exists($filePath) || file_exists($filePath . '.php')) {
+        file_exists($filePath . '.php') ? $filePath = $filePath . '.php' : null;
+        include $filePath;
+    } else {
+        // Handle the case when the file doesn't exist or is not a PHP file
+        // You might want to show a 404 error or redirect to a default page
+        include '404-page.html';
+        header("HTTP/1.0 404 Not Found");
+        echo "404 - Page not found";
+    }
+}
+?>
