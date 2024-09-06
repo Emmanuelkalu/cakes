2a2ec768c46c68a133ba0d86954ac24aff83ec3d
Henschel <henschly@gmail.com>
2024-09-06 07:59:31 +0100

    added commenrt

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (empty($filePath)) {
         include $filePath;
     } else {
         // Handle the case when the file doesn't exist or is not a PHP file
-        // You might want to show a 404 error or redirect to a default page
+        // You might want to show a 404 error or redirect to a default page smaple conmnent
         include '404-page.html';
         header("HTTP/1.0 404 Not Found");
         echo "404 - Page not found";
