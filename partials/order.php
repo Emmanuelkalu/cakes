cfc0ae04d845e52192d1f0f857087cc698c36a1a
Henschel <henschly@gmail.com>
2024-09-13 01:59:12 +0100

    updated form

--- partials/order.php
+++ partials/order.php
@@ -1,4 +1,4 @@
-<form class="ps-form--menu ps-form--order-form" action="https://nouthemes.net/html/bready/do_action" method="post">
+<form id="orderForm" class="ps-form--menu ps-form--order-form" action="https://nouthemes.net/html/bready/do_action" method="post">
     <div class="ps-section__header text-center">
         <h3 class="ps-section__title">Order Form</h3>
         <p>Making people happy</p><span><img src="images/icons/floral.png" alt=""></span>
@@ -7,74 +7,205 @@
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
             <div class="form-group">
                 <label>Your Name <sup>*</sup></label>
-                <input class="form-control" type="text" placeholder="">
+                <input name="name" class="form-control" type="text" placeholder="">
             </div>
         </div>
+
+        
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Email Address <sup>*</sup></label>
+                <input name="emailAddress" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+    
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Date  <sup>*</sup></label>
+                <div class="ps-form--icon"><i class="fa fa-calendar-check-o"></i>
+                    <input name="date" class="form-control" type="text" placeholder="">
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Best time we contact you * <sup>*</sup></label>
+                <div class="ps-form--icon"><i class="fa fa-clock-o"></i>
+                    <input name="time" class="form-control" type="text" placeholder="">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Delivery Address <sup>*</sup></label>
+                <input name="deliveryAddress" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+        
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
             <div class="form-group">
                 <label>Phone Number <sup>*</sup></label>
-                <input class="form-control" type="text" placeholder="">
+                <input name="phoneNumber" class="form-control" type="text" placeholder="">
             </div>
         </div>
+        
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
             <div class="form-group">
-                <label>Your Address <sup>*</sup></label>
-                <select class="form-control" type="text">
-                    <option value="">Select Your Address</option>
-                    <option value="123 Street">123 Street</option>
-                    <option value="456 Avenue">456 Avenue</option>
-                    <option value="789 Boulevard">789 Boulevard</option>
-                    <option value="1011 Lane">1011 Lane</option>
-                    <option value="1213 Drive">1213 Drive</option>
-                    <option value="1415 Road">1415 Road</option>
-                    <option value="1617 Place">1617 Place</option>
-                    <option value="1819 Court">1819 Court</option>
-                    <option value="2021 Square">2021 Square</option>
-                    <option value="2223 Circle">2223 Circle</option>
+                <label>Type of Event <sup>*</sup></label>
+                <select name="typeOfEvent" class="form-control" type="text">
+                    <option value=""></option>
+                    <option value="Birthday">Birthday</option>
+                    <option value="Wedding">Wedding</option>
+                    <option value="Party">Party</option>
+                  
                 </select>
             </div>
         </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Cake Colors <sup>*</sup></label>
+                <input name="cakeColors" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Buttercream Flavors <sup>*</sup></label>
+                <input name="buttercreamFlavors" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Fillings Flavors <sup>*</sup></label>
+                <input name="fillingsFlavors" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Our Flavors Combination<sup>*</sup></label>
+                <select name="ourFlavorsCombination" class="form-control" type="text">
+                    <option value=""></option>
+                    <option value="Classic Velvet">Classic Velvet</option>
+                    <option value="Strawberry Flavor">Strawberry Flavor</option> 
+                </select>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Number of Tiers <sup>*</sup></label>
+                <input name="numberOfTiers" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Number of Servings <sup>*</sup></label>
+                <input name="numberOfServings" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>What is your budget? <sup>*</sup></label>
+                <input name="yourBudget" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Doughnuts<sup>*</sup></label>
+                <select name="doughnuts" class="form-control" type="text">
+                    <option value=""></option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option> 
+                </select>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Macarons<sup>*</sup></label>
+                <select name="macarons" class="form-control" type="text">
+                    <option value=""></option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option> 
+                </select>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Frosted Cupcakes<sup>*</sup></label>
+                <select name="frostedCupcakes" class="form-control" type="text">
+                    <option value=""></option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option> 
+                </select>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Buttercream Cupcakes<sup>*</sup></label>
+                <select name="butterCreamCupcakes" class="form-control" type="text">
+                    <option value=""></option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option> 
+                </select>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Fondant Decorated Cupcakes<sup>*</sup></label>
+                <select name="fondantDecoratedCupcakes" class="form-control" type="text">
+                    <option value=""></option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option> 
+                </select>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Other Details (Flowers, Toppers)<sup>*</sup></label>
+                <input name="otherDetails" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <div class="form-group">
+                <label>Subject<sup>*</sup></label>
+                <input name="subject" class="form-control" type="text" placeholder="">
+            </div>
+        </div>
+
+    
+            
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
             <div class="form-group">
                 <label>Image <sup>*</sup></label>
                 <input class="form-control" type="file" accept="image/*" placeholder="">
             </div>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
-            <div class="form-group">
-                <label>Choose the date you want order bakery * <sup>*</sup></label>
-                <div class="ps-form--icon"><i class="fa fa-calendar-check-o"></i>
-                    <input class="form-control" type="text" placeholder="">
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
-            <div class="form-group">
-                <label>Best time we contact you * <sup>*</sup></label>
-                <div class="ps-form--icon"><i class="fa fa-clock-o"></i>
-                    <input class="form-control" type="text" placeholder="">
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
-            <div class="form-group">
-                <label>Name Bakery</label>
-                <input class="form-control" type="email" placeholder="">
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
-            <div class="form-group">
-                <label>Quanlity Bakery</label>
-                <input class="form-control" type="email" placeholder="">
-            </div>
-        </div>
+                      
+      
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
             <div class="form-group">
                 <label>Notes about your order, e.g. special notes for delivery.</label>
-                <textarea class="form-control" rows="8" placeholder=""></textarea>
+                <textarea name="enterMessage" class="form-control" rows="8" placeholder=""></textarea>
             </div>
             <div class="form-group submit">
-                <button class="ps-btn ps-btn--yellow">Order Bakery Now</button>
+                <button class="ps-btn ps-btn--yellow">Order Now</button>
             </div>
         </div>
     </div>
