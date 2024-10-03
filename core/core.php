ccc030bd3d79ca19a23802f2ee32368a122de206
Henry Chida <123965349+techchida@users.noreply.github.com>
2024-10-03 18:46:01 +0100

    upda core

--- core/core.php
+++ core/core.php
@@ -1,5 +1,4 @@
 <?php session_start();
-ini_set("display_errors", 1);
 // Database connection
 // $conn = new mysqli("localhost", "root", "", "buccie_cake");
 $conn = new mysqli("localhost", "buccie_cake", "Bitmonster11#", "buccie_cake");
