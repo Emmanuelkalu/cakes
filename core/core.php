46fe126f66594ce321562153943fbc78227d1fc2
Henry Chida <123965349+techchida@users.noreply.github.com>
2024-10-04 14:07:47 +0100

    main

--- core/core.php
+++ core/core.php
@@ -6,11 +6,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// $stripeSecretKey = "sk_test_51IkbSnH2j0YsDFbqDTF26kmj01zmNODrsJcHNJvv9IfttQknv7FjJtrkQBSbyXd17OgPWfn7dQ39OfrX2APlRgBA00PWDKN0Ju";
-// $stripe_publishable_key = "pk_test_51IkbSnH2j0YsDFbqFg6aHScfvgfXh9X1sCEyLDUpGBANCcEgQtmvyDiP4khGxYgKlIULxpF76mDwz08oZwJdLla400ESavFqH9";
+$stripeSecretKey = "sk_test_51IkbSnH2j0YsDFbqDTF26kmj01zmNODrsJcHNJvv9IfttQknv7FjJtrkQBSbyXd17OgPWfn7dQ39OfrX2APlRgBA00PWDKN0Ju";
+$stripe_publishable_key = "pk_test_51IkbSnH2j0YsDFbqFg6aHScfvgfXh9X1sCEyLDUpGBANCcEgQtmvyDiP4khGxYgKlIULxpF76mDwz08oZwJdLla400ESavFqH9";
 
-$stripeSecretKey = "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";
-$stripe_publishable_key = "pk_live_51IkbSnH2j0YsDFbqNtkTtrCUQA1m8FtCN1tCI9Z0pT7chzujPLL7qnFFvO39WnGr8xsrg0MKw9rJOJSM3iB98N6f00lgxb7UNr";
+// $stripeSecretKey = "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";
+// $stripe_publishable_key = "pk_live_51IkbSnH2j0YsDFbqNtkTtrCUQA1m8FtCN1tCI9Z0pT7chzujPLL7qnFFvO39WnGr8xsrg0MKw9rJOJSM3iB98N6f00lgxb7UNr";
 
 // Check if the cart session is not set, and if so, initialize it as an empty array
 if (!isset($_SESSION['cart'])) {
